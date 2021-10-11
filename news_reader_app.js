f67349d8069cef6c19ea3d7cc048bb5347fe78a5
Riya <71124402+riya-spec@users.noreply.github.com>
2021-10-11 19:13:09 +0530

    search by source

--- news_reader_app.js
+++ news_reader_app.js
@@ -1,4 +1,22 @@
 // Use this sample to create your own voice commands
-intent('hello world', p => {
-    p.play('(hello|hi there)');
-});
+intent('What does this app do?', 'What can I do here?',
+      reply('This is a news project.'));
+
+//intent('Start a command', (p) => {
+//    p.play({ command: 'testCommand'});
+//})
+
+const API_KEY = 'b99e8cf989054638a58b4262e3c0b45f';
+
+//News by source
+intent('Give me the news from $(source* (.*))', (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
+    
+    if(p.source.value) {
+        NEWS_API_URL = ` ${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
+    }
+    api.request(NEWS_API_URL,(error,response,body) => {
+        const {articles} = JSON.parse(body);
+    });
+    
+})
